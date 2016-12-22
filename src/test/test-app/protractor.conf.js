5d0f4e4092b75c4544cf367df6b68a5c2a9e3c45
Gabor Szmetanko <gabor.szmetanko@corballis.ie>
2016-12-22 22:05:51 +0100

    Travis E2E

--- src/test/test-app/protractor.conf.js
+++ src/test/test-app/protractor.conf.js
@@ -12,12 +12,24 @@ var config = {
 if (process.env.TRAVIS) {
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-  config.capabilities = {
-    'name': 'pageobject-generator node v' + process.env.TRAVIS_NODE_VERSION,
-    'browserName': 'chrome',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER
-  };
+  config.multiCapabilities = [
+    {
+      'browserName': 'chrome',
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      'build': process.env.TRAVIS_BUILD_NUMBER,
+      'version': 'latest',
+      "videoUploadOnPass": false,
+      "recordScreenshots": false
+    },
+    {
+      'browserName': 'chrome',
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      'build': process.env.TRAVIS_BUILD_NUMBER,
+      'version': 'latest-1',
+      "videoUploadOnPass": false,
+      "recordScreenshots": false
+    }
+  ];
 }
 
 
