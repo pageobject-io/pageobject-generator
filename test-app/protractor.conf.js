dd776b1f0653a48a8e8301b40ec9bd6c822ca384
Gabor Szmetanko <gabor.szmetanko@corballis.ie>
2017-07-11 23:02:04 +0200

    Re-added Sauce tests

--- test-app/protractor.conf.js
+++ test-app/protractor.conf.js
@@ -12,21 +12,22 @@ var config = {
 if (process.env.TRAVIS) {
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-  config.multiCapabilities = [{
-    'name': 'pageobject chrome latest',
-    'browserName': 'chrome',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'version': 'latest'
-  }
+  config.multiCapabilities = [
+  //   {
+  //   'name': 'pageobject chrome latest',
+  //   'browserName': 'chrome',
+  //   'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+  //   'build': process.env.TRAVIS_BUILD_NUMBER,
+  //   'version': 'latest'
+  // }
     // },
-    // {
-    //   'name': 'pageobject firefox 47',
-    //   'browserName': 'firefox',
-    //   'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    //   'build': process.env.TRAVIS_BUILD_NUMBER,
-    //   'version': '47'
-    // }
+    {
+      'name': 'pageobject firefox 47',
+      'browserName': 'firefox',
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      'build': process.env.TRAVIS_BUILD_NUMBER,
+      'version': '47'
+    }
   ];
 }
 
